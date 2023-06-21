e9397c730fef5effc585eb069964165318b37078
NopeRoms <133530462+NopeRoms@users.noreply.github.com>
2023-06-21 20:57:58 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -13,8 +13,8 @@ rm -rf prebuilts/clang/host/linux-x86/clang-r450784d
 rm -rf system/extras/su
 git clone https://gitlab.com/ImSurajxD/clang-r450784d prebuilts/clang/host/linux-x86/clang-r450784d --depth=1
 git clone https://github.com/NopeNopeGuy/device_xiaomi_whyred-1 device/xiaomi/whyred --depth=1 -b pe-13
-git clone https://github.com/NopeNopeGuy/vendor_xiaomi_whyred-1/ vendor/xiaomi/whyred --depth=1
-git clone https://github.com/NopeNopeGuy/android_kernel_xiaomi_whyred kernel/xiaomi/whyred -b KCUF_419 --depth=1 --recurse-submodules
+git clone https://github.com/NopeNopeGuy/vendor_xiaomi_whyred-1/ vendor/xiaomi/whyred --depth=1 -b thirteen-4.19
+git clone https://github.com/NopeNopeGuy/android_kernel_xiaomi_scarlet kernel/xiaomi/whyred -b a13/qti-haptics --depth=1
 git clone https://github.com/shekhawat2/android_hardware_qcom_media hardware/qcom-caf/msm8998/media --depth=1
 git clone https://github.com/shekhawat2/android_hardware_qcom_audio hardware/qcom-caf/msm8998/audio --depth=1
 git clone https://github.com/shekhawat2/android_hardware_qcom_display hardware/qcom-caf/msm8998/display --depth=1
